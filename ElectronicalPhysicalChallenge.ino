2e62a669356f5eb7b4305cd1d5a9c98673fbd0c0
idankri <58448747+idankri@users.noreply.github.com>
2020-11-30 23:06:44 +0200

    Added mode select functionality and improved documentation

--- ElectronicalPhysicalChallenge.ino
+++ ElectronicalPhysicalChallenge.ino
@@ -1,3 +1,15 @@
+/**
+ * @author: Idan Kringel and Noy
+ * The next project is an 8 bit Theremin.
+ * The instrument detects IR retransmission intensity (the threshold intensity is set by the room but usually works better with lights off),
+ *  and plays a note according to that intensity - meaning that the closer your hand the higher the note played.
+ * The instrument plays notes in a rapid pace, set by tempo variable, and the tempo can be changed pressing the left button - long press yields slower tempo and
+ *   fast press yields faster tempo
+ * Also, the right button can change the mode of the notes (Modus) - the instrument initially plays C Major scale, and each press switches to C in the next mode -  
+ *   Dorian - Phrygian - Lydian - Mixolydian - Aeolian (Minor) - Locrian - and back to Ionian\Major
+ * Video Link - <TODO: UPLOAD VIDEO LINK>
+ */
+
 //Imports
 #include <Adafruit_CircuitPlayground.h>
 #include <stdlib.h>
@@ -11,14 +23,24 @@ void setIR();
 int readIR();
 void readRoom();
 void setRandomNeoPixels();
+void getNewTempo();
+void getNewMode();
 
 //global variables
 int ROOM_DEFAULT = 0;
 int IR_res;
 int neoPixelsParity = 0;
-int NOTES [8] = {261, 293, 329, 349, 391, 440, 493, 523}; // C-D-E-F-G-A-B-C
-bool leftButtonDown = false;
-int tempo = 20;
+int tempo = 20; // sets the delay between loops
+int mode = 0; //mode for music mode (modus)
+int NOTES [7][8] = {{261, 293, 329, 349, 391, 440, 493, 523},
+                    {261, 293, 311, 349, 391, 440, 466, 523},
+                    {261, 277, 311, 349, 391, 415, 466, 523},
+                    {261, 293, 329, 369, 391, 440, 493, 523},
+                    {261, 293, 329, 349, 391, 440, 466, 523},
+                    {261, 293, 311, 349, 391, 415, 466, 523},
+                    {261, 277, 311, 349, 369, 415, 466, 523}}; // All basic modes for C, first one is C-D-E-F-G-A-B-C
+
+
 
 // the next two functions are modifications of proximity library adapted to the projects needs
 void setIR(){
@@ -44,9 +66,9 @@ int readIR(){
   digitalWrite(25, LOW);
   //Make reading a more reasonable number
   Serial.println(IR);
-  IR = map(IR, ROOM_DEFAULT, 980, 0, ARRAY_SIZE(NOTES) - 1);
-  if(IR >= ARRAY_SIZE(NOTES)){
-    IR = ARRAY_SIZE(NOTES) - 1;
+  IR = map(IR, ROOM_DEFAULT, 980, 0, ARRAY_SIZE(NOTES[0]) - 1);
+  if(IR >= ARRAY_SIZE(NOTES[0])){
+    IR = ARRAY_SIZE(NOTES[0]) - 1;
   }
   else if(IR < 0){
     IR = 0;
@@ -75,16 +97,9 @@ void setRandomNeoPixels(int parity){
    */
   int i;
   for(i = parity; i < 10; i+=2){
-    CircuitPlayground.setPixelColor(i, RANDOM(0x000000, 0xFFFFFF));
+    // generating LED values with less red doesn't look as good but prevents bugs for IR detection
+    CircuitPlayground.setPixelColor(i, RANDOM(0x000000, 0x11FFFF)); 
   }
-}
-
-void setup() {
-  CircuitPlayground.begin();
-  while (!Serial);
-  Serial.begin(19200);
-  setIR();
-  CircuitPlayground.clearPixels();
 }
 
 void getNewTempo(){
@@ -97,26 +112,46 @@ void getNewTempo(){
     delay(1);
     ++count;
   }
-  Serial.println(count);
-  tempo = count / 10;
+  // Experimentation led me to the conclusion that this formula yields a "tempo" reasonable to the length of the press
+  tempo = (count - 350 > 10)? count - 350: 10;  
+}
+
+void getNewMode(){
+  /**
+   * When right button is up (after being down) switches mode (Modus \ scale) to the next mode.
+   */
+  while(CircuitPlayground.rightButton()){
+    delay(1);
+  }
+  mode = (mode + 1) % ARRAY_SIZE(NOTES);
+}
+
+
+void setup() {
+  CircuitPlayground.begin();
+  while (!Serial);
+  Serial.begin(19200);
+  setIR();
+  CircuitPlayground.clearPixels();
 }
 
 void loop() {
   IR_res = readIR();
-  // prints IR reading for debugging purposes
-  //Serial.println(IR_res);
-  //every iteration of the loop neoPixelsParity will be 0 or 1
-  neoPixelsParity = (neoPixelsParity + 1) % 2;
+  //every iteration of the loop neoPixelsParity will be flipped between 0 and 1
+  neoPixelsParity = neoPixelsParity ^ 1;
   // play random lights and play tone according to reading
   setRandomNeoPixels(neoPixelsParity); 
-  CircuitPlayground.playTone(NOTES[IR_res], 100);
-  //clear pixels and have some delay for robustness purposes
+  CircuitPlayground.playTone(NOTES[mode][IR_res], 100);
   CircuitPlayground.clearPixels();
-  // The next form of loop makes button functionality more responsive
+  // The next form of loop makes button functionality more responsive in a trade off for less accurate "tempo"
   for(int i = 0; i < tempo; i++){
     delay(1);
     if(CircuitPlayground.leftButton()){
       getNewTempo();
+      break;
+    }
+    if(CircuitPlayground.rightButton()){
+      getNewMode();
       break;
     }
   }
